525691ac60a3980faf6d4f6ee1ba9fa0cdae220c
Najah Farah <61600465+nfarah22@users.noreply.github.com>
2024-07-29 01:57:22 +0100

    did rebuild

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    REACT_APP_BACKEND_URL: 'https://full-stack-project-assessment-t10z.onrender.com',
+    REACT_APP_BACKEND_URL:'https://full-stack-project-assessment-t10z.onrender.com',
   };
   
   export default config;
